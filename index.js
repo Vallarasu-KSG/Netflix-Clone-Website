99383d467c11cfa71340f5f3cf89e3b4a46dd515
Vallarasu-KSG <kvallarasu@2003.com>
2024-09-30 08:50:27 +0530

    create-website-netflix-clone

---
+++ index.js
@@ -0,0 +1,52 @@
+// languageButton
+// const languageButton = document.querySelector('.language-button');
+// const languageDropdown = document.querySelector('.language-dropdown');
+
+// languageButton.addEventListener('click', () => {
+//     languageDropdown.style.display = languageDropdown.style.display === 'none' ? 'block' : 'none';
+// });
+const carousel = document.querySelector(".carousel");
+const arrowBtns = document.querySelectorAll('.wrapper svg');
+const firstCard = carousel.querySelector(".card-img");
+const firstCardWidth = firstCard.offsetWidth;
+
+let scrollPosition = 0;
+
+arrowBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    console.log(btn.id);
+    const direction = btn.id === "left" ? -1 : 1;
+    scrollPosition += direction * firstCardWidth;
+    carousel.scrollLeft = scrollPosition;
+  });
+});
+
+
+
+let accordian = document.querySelectorAll(".question");
+
+for (let i = 0; i < accordian.length; i++)
+{
+  accordian[i].addEventListener("click", function () {
+    if (this.childNodes[1].classList.contains("fa-plus"))
+    {
+      this.childNodes[1].classList.remove("fa-plus");
+      this.childNodes[1].classList.add("fa-plus");
+    }
+    else
+    {
+      this.childNodes[1].classList.remove("fa-plus");
+      this.childNodes[1].classList.add("fa-plus");
+    }
+
+    let content = this.nextElementSibling;
+    if (content.style.maxHeight)
+    {
+      content.style.maxHeight = null;
+    }
+    else
+    {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}

2475e9f48db917e4d88f4f04cc27153c5c81101c
Vallarasu-KSG <kvallarasu@2003.com>
2024-10-01 12:32:13 +0530

    javascript-update

---
+++ sign-in-2.js
@@ -0,0 +1,179 @@
+
+const login2 = document.querySelectorAll(".Login2");
+const signup2 = document.querySelectorAll(".signup");
+// const forget2 = document.querySelectorAll(".forget");
+const signuplink = document.querySelectorAll("#signuplink");
+const signinlink = document.querySelectorAll("#signinlink");
+// const forget = document.querySelectorAll(".forget2");
+const returen2 = document.querySelectorAll("#return");
+
+// Function to handle signup link click
+function signup(event) {
+  event.preventDefault();
+  login2.forEach((element) => {
+    element.style.visibility = "hidden";
+  });
+  signup2.forEach((element) => {
+    element.style.visibility = "visible";
+  });
+  forget2.forEach((element) => {
+    element.style.visibility = "hidden";
+  });
+}
+
+// Function to handle signin link click
+function signin(event) {
+  event.preventDefault();
+  login2.forEach((element) => {
+    element.style.visibility = "visible";
+  });
+  signup2.forEach((element) => {
+    element.style.visibility = "hidden";
+  });
+  forget2.forEach((element) => {
+    element.style.visibility = "hidden";
+  });
+  forget.forEach((element) => {
+    element.style.visibility = "visible";
+  });
+}
+
+// Function to handle forgot password link click
+// function ForgotPassword(event) {
+//   event.preventDefault();
+//   login2.forEach((element) => {
+//     element.style.visibility = "hidden";
+//   });
+//   signup2.forEach((element) => {
+//     element.style.visibility = "hidden";
+//   });
+//   forget2.forEach((element) => {
+//     element.style.visibility = "visible";
+//   });
+//   forget.forEach((element) => {
+//     element.style.visibility = "hidden";
+//   });
+  
+// }
+
+// function returen(event)
+// {
+//     event.preventDefault();
+//     login2.forEach((element) => {
+//       element.style.visibility = "hidden";
+//     });
+//     signup2.forEach((element) => {
+//       element.style.visibility = "visible";
+//     });
+//     forget2.forEach((element) => {
+//       element.style.visibility = "hidden";
+//     });
+//   }
+
+
+// Add event listeners to signup and signin links
+signuplink.forEach((element) => {
+  element.addEventListener("click", signup);
+});
+
+signinlink.forEach((element) => {
+  element.addEventListener("click", signin);
+});
+
+// Add event listener to forgot password link
+// forget2.forEach((element) => {
+//   element.addEventListener("click", ForgotPassword);
+// });
+
+// returen2.forEach((element) => {
+//     element.addEventListener("click", returen);
+//   });
+// end
+
+
+var emailarray=[];
+var passwordarray=[];
+
+function signinbtn()
+{
+
+    event.preventDefault();
+    var email = document.getElementById("se").value;
+    var password = document.getElementById("sp").value;
+    // array strat from 0
+    var i = emailarray.indexOf(email);
+    if (emailarray.indexOf(email) == -1) // this if value irukkanu check pannum
+    {
+        if (email < 1) //this is nested if //this if register panna value ah kuthu irukkingalanu check pannum
+        {
+            alert("Email Required.");
+            return;
+        }
+        alert(" Emial does not match.");
+        return;
+    }
+    else if (passwordarray[i] != password)
+    {
+        if (password < 1)
+        {
+            alert("Password Required.");
+            return;
+        }
+        alert("Password does not match.");
+        return;
+    }
+    else
+    {
+        
+        alert(email + "Login Successful Welcome");
+        window.open('index.html','_self');
+        document.getElementById('se').value=" ";
+        document.getElementById('sp').value=" ";
+    }
+}
+
+function signupbtn()
+{
+
+    event.preventDefault();
+    
+    var email = document.getElementById("re").value;
+    var password = document.getElementById("rp").value;
+    var passwordRetype = document.getElementById("rrp").value;
+
+    if (email < 1)
+    {
+        alert("Email required.");
+        return;
+    }
+    else if (password < 1)
+    {
+        alert("Password required.");
+        return;
+    }
+    else if (passwordRetype < 1)
+    {
+        alert("passwordRetype required.");
+        return;
+    }
+    else if (password != passwordRetype)
+    {
+        alert("password does not match");
+        return;
+    }
+    else if (emailarray.indexOf(email) == -1)
+    {
+        emailarray.push(email);
+        passwordarray.push(password);
+        alert(email + "Thank for registration.");
+
+        // clear-value
+        document.getElementById("re").value =" ";
+        document.getElementById("rp").value =" ";
+        document.getElementById("rrp").value =" ";
+    }
+}
+// function forgetbtn()
+// {
+
+// }
